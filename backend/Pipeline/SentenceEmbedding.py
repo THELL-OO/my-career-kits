9941969e597330051df3296edf95713482d81712
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-02-25 22:46:21 +0545

    Update SentenceEmbedding.py

--- backend/Pipeline/SentenceEmbedding.py
+++ backend/Pipeline/SentenceEmbedding.py
@@ -1,31 +1,27 @@
 import logging, os, spacy
 from sentence_transformers import SentenceTransformer
 
-nlp = spacy.load('en_core_web_trf')
-
 logging.getLogger("transformers").setLevel(logging.ERROR)
-logging.getLogger("sentence_transformers").setLevel(logging.ERROR)
 
-model_path = 'models/mpnet_local'
+nlp = spacy.blank("en")
+nlp.add_pipe("sentencizer")
+
+model_path = "models/mpnet_local"
 
 if os.path.exists(model_path):
-   model = SentenceTransformer(model_path, device='cpu')
-
+    model = SentenceTransformer(model_path, device="cpu")
 else:
-  model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
-  model.save(model_path)
+    model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2", device="cpu")
+    model.save(model_path)
 
 
 def sentence_embedding(text):
-  doc = nlp(text)
-  sentence_list = [sent.text.strip() for sent in doc.sents]
-  sentence_vector = model.encode(
-                      sentence_list, 
-                      output_value='sentence_embedding',
-                      convert_to_numpy=True,
-                      convert_to_tensor=False,
-                      batch_size=24,
-                      normalize_embeddings=True,
-                      device='cpu'
-                    )
-  return sentence_vector
+    doc = nlp(text)
+    sentence_list = [sent.text.strip() for sent in doc.sents]
+
+    return model.encode(
+        sentence_list,
+        convert_to_numpy=True,
+        batch_size=16,
+        normalize_embeddings=True
+    )
