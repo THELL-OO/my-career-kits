67148664313f7b9473023b26839ae150bc70d978
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-02-26 00:21:17 +0545

    Update resume_router.py

--- backend/resume_router.py
+++ backend/resume_router.py
@@ -14,12 +14,12 @@ async def resume(resume_file: UploadFile = File(...)):
    try:  
       raw_text = await resume_file.read()
       score= resume_pipeline(raw_text, resume_file)
-      score = int(score*100)
+      score_percentage = int(score * 100)
 
       return ResumeResponse(
          filename = resume_file.filename,
          size = resume_file.size,
-         score = score,
+         score = score_percentage,
          message = 'resume_file received successfully!'
       )
    
